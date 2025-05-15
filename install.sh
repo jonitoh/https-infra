562d82d15520a869ceaca74727a0b7a946469f07
jonitoh <jordannt94@gmail.com>
2025-05-16 01:32:45 +0200

    :construction_worker: build(minor): add .gitignore

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@
 # Script to make the VM work with the 
 # reverse-proxy and the services
 # ----------------------------------------------
-# CREATE the network web
+# CREATE the network infra-web
 # https://www.linuxbash.sh/post/managing-docker-networks-with-bash-scripts
-if [ "$(docker network ls | grep web)" ]; then
-    echo "Network web already exists."
+if [ "$(docker network ls | grep infra-web)" ]; then
+    echo "Network infra-web already exists."
 else
-    docker network create web
-    echo "Network web created."
+    docker network create infra-web
+    echo "Network infra-web created."
 fi
